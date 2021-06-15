19d03b95dacaa49419fa6342a95f2208b96391b7
MallardOfGlory <65434066+MallardOfGlory@users.noreply.github.com>
2021-06-15 13:42:50 +1000

    Updated file
    its updated with replies

--- twitterforwhatever.py
+++ twitterforwhatever.py
@@ -1,7 +1,8 @@
 import tweepy
 
-message = "Hello"
+message = ("media test")
 twitter_id = "1403631556799787021"
+media = r"C:\Users\Epicd\Pictures\on the outside.JPG"
 #This is where you need to put all your keys
 #get them from https://developer.twitter.com/
 
@@ -19,7 +20,14 @@ api = tweepy.API(thingy)
 #This is where you can tweet what you want
 #replace twitter_id with the tweet id and run the program
 #THIS IS CURRENTLY SET UP TO TWEET AND REPLY TO A TWEET
-#to just tweet, add a # infront of the second line (line 25)
-#to reply to a message, add a # infront of the first line (line 24)
-api.update_status(status=message)
-reply_status = api.update_status(status = message, in_reply_to_status_id =twitter_id, auto_populate_reply_metadata=True)
+#to just tweet, add a # infront of line 31
+#to reply to a message, add a # infront of line 33
+#images only work for tweets right now, I want to add them to replies but I also want to play fortnite
+#For media, remove the # for line 29 and add # for lines 31 and 33
+
+#Gets image and tweet and shoves them together
+status = api.update_with_media(message, media)
+#replies to a tweet
+#reply_status = api.update_status(status = message, in_reply_to_status_id =twitter_id, auto_populate_reply_metadata=True)
+#Tweets out (only use with replying/normally tweeting, dont use when you want to tweet an image)
+#api.update_status(status=message)
